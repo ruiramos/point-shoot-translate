a8674c60b74f095d81158b7057983a6a4cbdd576
Rui Ramos <rui@ometria.com>
2015-01-25 22:28:26 +0000

    scroll issues

--- app/components/PhotoContainer.jsx
+++ app/components/PhotoContainer.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var PhotoContainer = React.createClass({
 
   componentWillUpdate: function(){
-    if(this.props.imageUrl){
+    if(this.props.imageUrl && this.props.imageUrl.length){
       $("html, body").animate({ scrollTop: $('#photo-container').offset().top - 20 });
+    } else {
+      $("html, body").scrollTop(0);
     }
   },
 
